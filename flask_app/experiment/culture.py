be80a94977540779942bb7c13854f099d5617e3a
Catalin Rusnac <ctlnrsnc@gmail.com>
2024-06-27 02:53:27 +0200

    none check

--- flask_app/experiment/culture.py
+++ flask_app/experiment/culture.py
@@ -294,9 +294,9 @@ class Culture:
         if include_current and self.new_culture_data is not None:
             od_dict[self.new_culture_data.timestamp] = self.new_culture_data.od  # Include current uncommitted data
 
-        od_dict = {k: v for k, v in od_dict.items() if v is not None}
-        mu_dict = {k: v for k, v in mu_dict.items() if v is not None}
-        rpm_dict = {k: v for k, v in rpm_dict.items() if v is not None}
+        od_dict = {k: v for k, v in od_dict.items() if v is not None and k is not None}
+        mu_dict = {k: v for k, v in mu_dict.items() if v is not None and k is not None}
+        rpm_dict = {k: v for k, v in rpm_dict.items() if v is not None and k is not None}
 
         od_dict = {k: v for k, v in sorted(od_dict.items(), key=lambda item: item[0])}
         mu_dict = {k: v for k, v in sorted(mu_dict.items(), key=lambda item: item[0])}
