f79ef2961fdb4ca565d3b3558cdf77e4fbc604de
Catalin Rusnac <ctlnrsnc@gmail.com>
2025-01-29 13:41:27 +0100

    stirrer acceleration

--- flask_app/minimal_device/stirrers.py
+++ flask_app/minimal_device/stirrers.py
@@ -59,7 +59,7 @@ class Stirrers:
             duty_cycle = self.device.device_data["stirrers"]["calibration"][vial][speed]
 
         if 0 < duty_cycle < 0.5 and accelerate:
-            accelerate_duty_cycle = self.device.device_data["stirrers"]["calibration"][vial]["high"] * 2
+            accelerate_duty_cycle = self.device.device_data["stirrers"]["calibration"][vial]["high"] * 1.5
             accelerate_duty_cycle = min(accelerate_duty_cycle, 1)
             self._set_duty_cycle(vial, accelerate_duty_cycle)
             time.sleep(0.05)
