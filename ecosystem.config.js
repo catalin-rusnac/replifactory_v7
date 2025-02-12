f3ca29367e574ed17d34cb681d4b276ceba68355
Catalin Rusnac <ctlnrsnc@gmail.com>
2025-02-12 20:13:35 +0100

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,14 +4,14 @@ module.exports = {
       name: "backend",
       script: "uv",
       args: "run server.py",
-      cwd: "~/replifactory_v7/flask_app",
+      cwd: "/home/pi/replifactory_v7/flask_app",
       autorestart: true,
       watch: false
     },
     {
       name: "frontend",
-      script: "~/replifactory_v7/vue/src/server/express_server.js",
-      cwd: "~/replifactory_v7/vue",
+      script: "/home/pi/replifactory_v7/vue/src/server/express_server.js",
+      cwd: "/home/pi/replifactory_v7/vue",
       autorestart: true,
       watch: false
     }
